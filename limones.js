4483c19ed8470eda2df25f385ccd2da77063043c
Carlinux189 <carlosj_1912_17@hotmail.com>
2026-04-09 17:17:14 -0500

    mover a la izquierda

--- limones.js
+++ limones.js
@@ -3,6 +3,7 @@ let ctx = canvas.getContext("2d");
 const ALTURA_SUELO = 30;
 const ALTO_PERSONAJE = 60;
 const ANCHO_PERSONAJE = 40;
+let personajeX = canvas.width / 2;
 
 function iniciar() {
     dibujarSuelo();
@@ -16,5 +17,20 @@ function dibujarSuelo() {
 
 function dibujarPersonaje() {
     ctx.fillStyle = "yellow";
-    ctx.fillRect(canvas.width / 2, canvas.height - (ALTURA_SUELO + ALTO_PERSONAJE), ANCHO_PERSONAJE, ALTO_PERSONAJE);
+    ctx.fillRect(personajeX, canvas.height - (ALTURA_SUELO + ALTO_PERSONAJE), ANCHO_PERSONAJE, ALTO_PERSONAJE);
+}
+
+function moverIzquierda() {
+    personajeX -= 10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla() {
+    limpiarCanvas();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
