2fd51d1c89a19a0d14380bdcfb3ab41dc9cc41b9
Carlinux189 <carlosj_1912_17@hotmail.com>
2026-04-09 23:16:19 -0500

    agregar colisiones

--- limones.js
+++ limones.js
@@ -1,13 +1,14 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
-const ALTURA_SUELO = 30;
+const ALTURA_SUELO = 20;
 const ALTO_PERSONAJE = 60;
 const ANCHO_PERSONAJE = 40;
 const ANCHO_LIMON = 20;
 const ALTO_LIMON = 20;
 
 let personajeX = canvas.width / 2;
+let personajeY = canvas.height - (ALTURA_SUELO + ALTO_PERSONAJE);
 let limonX = canvas.width / 2;
 let limonY = 0;
 
@@ -30,11 +31,13 @@ function dibujarPersonaje() {
 function moverIzquierda() {
     personajeX -= 10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function moverDerecha() {
     personajeX += 10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function actualizarPantalla() {
@@ -56,4 +59,10 @@ function dibujarLimon() {
 function bajarLimon() {
     limonY += 10;
     actualizarPantalla();
+}
+
+function detectarColision() {
+    if (limonX + ANCHO_LIMON > personajeX && limonX < personajeX + ANCHO_PERSONAJE && limonY + ALTO_LIMON > personajeY && limonY < personajeY + ALTO_PERSONAJE) {
+        alert("Atrapado!!\u{1F601}");
+    }
 }
