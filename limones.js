ddfe9ba5eada485bede6ea8780f772c9d4bbcce4
Carlinux189 <carlosj_1912_17@hotmail.com>
2026-04-11 16:19:04 -0500

    agregar funcion SteInterval

--- limones.js
+++ limones.js
@@ -13,8 +13,10 @@ let limonX = canvas.width / 2;
 let limonY = 0;
 let puntaje = 0;
 let vidas = 3;
+let velocidadcaida = 200;
 
 function iniciar() {
+    setInterval(bajarLimon, velocidadcaida); //primerPrarametro: funcion, segundoParametro: tiempo en milisegundos
     dibujarSuelo();
     dibujarPersonaje();
     aparecerLimon();
